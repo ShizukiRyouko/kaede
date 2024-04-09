80903d5bef4b223c9a2f0b270ddc3a98314eb854
DESKTOP-EEIKLO4\fi <muhammadraflipembalap@gmail.com>
2024-04-09 18:33:53 +0700

    tiling prototype 1

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var eventModule = document.createElement('script');
 eventModule.src = "./lib/Event.js";
 document.head.appendChild(eventModule);
+
+
+
+const tileMap = document.createElement('script');
+tileMap.src = "./lib/TileMap.js";
+document.head.appendChild(tileMap);

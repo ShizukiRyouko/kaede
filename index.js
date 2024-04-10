b43bf79b48599c56b05663287222535acc30e17d
doremi <nuki@kaname.cloud>
2024-04-11 00:12:17 +0700

    importer

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-var eventModule = document.createElement('script');
-eventModule.src = "./lib/Event.js";
-document.head.appendChild(eventModule);
+function importModule (path) {
+  let root = "./lib/"
+  let eventModule = document.createElement('script');
+  eventModule.src = root + path ;
+  document.head.appendChild(eventModule);
+}
 
-
-
-const tileMap = document.createElement('script');
-tileMap.src = "./lib/TileMap.js";
-document.head.appendChild(tileMap);
-
-const playerScript = document.createElement('script');
-playerScript.src = "./lib/PlayerMovement.js";
-document.head.appendChild(playerScript);
-
-const teleportToLevel = document.createElement('script');
-teleportToLevel.src = "./lib/teleportToLevel.js";
-document.head.appendChild(teleportToLevel);
+importModule("PlayerMovement.js")
